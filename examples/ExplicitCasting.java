a8d59ff9006ccd9df4cf802b22645cd66f068bbb
domaakshaya-19 <2420030474@klh.edu.in>
2025-07-29 11:08:45 +0530

    added case study

---
+++ examples/ExplicitCasting.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class ExplicitCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double decimalNumber=9.78;
+		int wholeNumber;
+		wholeNumber=(int)decimalNumber;
+		System.out.println("Double value:"+decimalNumber);
+		System.out.println("Integer value after casting:"+wholeNumber);
+	}
+
+}
---
+++ examples/StringExample.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting ="Hello,World";
+		System.out.println(greeting);
+	}
+
+}
