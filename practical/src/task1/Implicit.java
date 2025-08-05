a11185b6d6fb9ec739efb73ba8b2a4c47a1f368e
domaakshaya-19 <2420030474@klh.edu.in>
2025-08-05 10:23:43 +0530

    Add files via upload

---
+++ practical/src/task1/Implicit.java
@@ -0,0 +1,17 @@
+package task1;
+
+public class Implicit {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intNumber=100;
+		long longNumber;
+		
+		 //Implicit Casting from smaller to larger
+		longNumber=intNumber;
+		System.out.println("Interger Value: " +intNumber);
+		System.out.println("Long Value After Implicit Casting: "+longNumber);
+		
+
+
+	}
+}
---
+++ practical/src/task1/Nonprimitive.java
@@ -0,0 +1,12 @@
+package task1;
+
+public class Nonprimitive {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String greeting="Hello World!!";
+		System.out.println(greeting);
+
+
+	}
+}
